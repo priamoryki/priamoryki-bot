6b4b8842b8386db1bd03dbd8eb7bdeba65850701
priamoryki <lymarpasha@gmail.com>
2024-04-17 22:48:16 +0300

    add Playlist base classes

---
+++ src/main/java/com/priamoryki/discordbot/api/database/entities/PlaylistSong.java
@@ -0,0 +1,49 @@
+package com.priamoryki.discordbot.api.database.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+/**
+ * @author Pavel Lymar
+ */
+@Entity()
+@Table(name = "playlist_song")
+public class PlaylistSong {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "url")
+    private String url;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
